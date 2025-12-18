df96dbf5bf8515f0426a718e3b39eb2d90d194da
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2025-12-18 23:40:21 +0300

    Create HappyPath

---
+++ src/sessions/CodeBeam.UltimateAuth.Sessions.InMemory/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CodeBeam.UltimateAuth.Core.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeBeam.UltimateAuth.Sessions.InMemory
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddUltimateAuthInMemorySessions(this IServiceCollection services)
+        {
+            services.AddSingleton<ISessionStoreFactory, InMemorySessionStoreFactory>();
+            return services;
+        }
+    }
+}
