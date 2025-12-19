bd2e09a55956fc28720d1d819ce3ec40a407c51c
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2025-12-20 00:36:42 +0300

    Add All Required Projects

---
+++ src/CodeBeam.UltimateAuth.Core/Contracts/Authority/AuthenticationContext.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeBeam.UltimateAuth.Core.Contracts
+{
+    public sealed record AuthenticationContext
+    {
+        public string? TenantId { get; init; }
+        public string Identifier { get; init; } = default!;
+        public string Secret { get; init; } = default!;
+        public AuthOperation Operation { get; init; } // Login, Reauth, Validate
+        public DeviceContext? Device { get; init; }
+    }
+}
