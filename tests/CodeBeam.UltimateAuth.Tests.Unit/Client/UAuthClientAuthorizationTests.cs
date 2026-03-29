b2000e64d35fdf0f58a619b46fc64dfc5c6d320e
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2026-03-29 15:45:33 +0300

    Added Client Session Tests

--- tests/CodeBeam.UltimateAuth.Tests.Unit/Client/UAuthClientAuthorizationTests.cs
+++ tests/CodeBeam.UltimateAuth.Tests.Unit/Client/UAuthClientAuthorizationTests.cs
@@ -8,6 +8,7 @@ using CodeBeam.UltimateAuth.Client.Services;
 using CodeBeam.UltimateAuth.Core.Contracts;
 using CodeBeam.UltimateAuth.Core.Defaults;
 using CodeBeam.UltimateAuth.Core.Domain;
+using CodeBeam.UltimateAuth.Core.MultiTenancy;
 using FluentAssertions;
 using Microsoft.Extensions.Options;
 using Moq;
@@ -144,5 +145,177 @@ public class UAuthClientAuthorizationTests
 
         result.IsSuccess.Should().BeTrue();
         result.Value.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetMyRoles_Should_Call_Correct_Endpoint_And_Return_Data()
+    {
+        var userKey = UserKey.FromString("user-1");
+
+        var response = new UserRolesResponse
+        {
+            UserKey = userKey,
+            Roles = new PagedResult<UserRoleInfo>(
+                new List<UserRoleInfo>
+                {
+                new UserRoleInfo
+                {
+                    Tenant = TenantKeys.Single,
+                    UserKey = userKey,
+                    RoleId = RoleId.From(Guid.NewGuid()),
+                    Name = "admin",
+                    AssignedAt = DateTimeOffset.UtcNow
+                }
+                },
+                totalCount: 1,
+                pageNumber: 1,
+                pageSize: 10,
+                sortBy: null,
+                descending: false)
+        };
+
+        _request.Setup(x => x.SendJsonAsync(It.IsAny<string>(), It.IsAny<object>()))
+            .ReturnsAsync(SuccessJson(response));
+
+        var client = CreateClient();
+
+        var result = await client.Authorization.GetMyRolesAsync();
+
+        _request.Verify(x =>
+            x.SendJsonAsync("/auth/me/authorization/roles/get", It.IsAny<object>()),
+            Times.Once);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value!.UserKey.Should().Be(userKey);
+        result.Value.Roles.Items.Should().HaveCount(1);
+        result.Value.Roles.Items[0].Name.Should().Be("admin");
+    }
+
+    [Fact]
+    public async Task GetUserRoles_Should_Call_Admin_Endpoint()
+    {
+        var userKey = UserKey.FromString("user-1");
+
+        var response = new UserRolesResponse
+        {
+            UserKey = userKey,
+            Roles = new PagedResult<UserRoleInfo>(
+                new List<UserRoleInfo>
+                {
+                new UserRoleInfo
+                {
+                    Tenant = TenantKeys.Single,
+                    UserKey = userKey,
+                    RoleId = RoleId.From(Guid.NewGuid()),
+                    Name = "admin",
+                    AssignedAt = DateTimeOffset.UtcNow
+                }
+                },
+                totalCount: 1,
+                pageNumber: 1,
+                pageSize: 10,
+                sortBy: null,
+                descending: false)
+        };
+
+        _request.Setup(x => x.SendJsonAsync(It.IsAny<string>(), It.IsAny<object>()))
+            .ReturnsAsync(SuccessJson(response));
+
+        var client = CreateClient();
+        var result = await client.Authorization.GetUserRolesAsync(userKey);
+        _request.Verify(x => x.SendJsonAsync($"/auth/admin/authorization/users/{userKey.Value}/roles/get", It.IsAny<object>()), Times.Once);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value!.UserKey.Should().Be(userKey);
+        result.Value.Roles.Items.Should().HaveCount(1);
+        result.Value.Roles.Items[0].Name.Should().Be("admin");
+    }
+
+    [Fact]
+    public async Task CreateRole_Should_Return_Result()
+    {
+        _request.Setup(x => x.SendJsonAsync(It.IsAny<string>(), It.IsAny<object>()))
+            .ReturnsAsync(SuccessJson(new RoleInfo() { Name = "admin" }));
+
+        var client = CreateClient();
+
+        var result = await client.Authorization.CreateRoleAsync(new CreateRoleRequest() { Name = "admin" });
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task RenameRole_Should_Publish_Event_On_Success()
+    {
+        _request.Setup(x => x.SendJsonAsync(It.IsAny<string>(), It.IsAny<object>()))
+            .ReturnsAsync(new UAuthTransportResult { Ok = true, Status = 200 });
+
+        var client = CreateClient();
+
+        var request = new RenameRoleRequest
+        {
+            Id = RoleId.From(Guid.NewGuid()),
+            Name = "new-role"
+        };
+
+        await client.Authorization.RenameRoleAsync(request);
+
+        _events.Verify(x => x.PublishAsync(It.Is<UAuthStateEventArgs>(e => e.Type == UAuthStateEvent.AuthorizationChanged)), Times.Once);
+    }
+
+    [Fact]
+    public async Task RenameRole_Should_NOT_Publish_Event_On_Failure()
+    {
+        _request.Setup(x => x.SendJsonAsync(It.IsAny<string>(), It.IsAny<object>()))
+            .ReturnsAsync(new UAuthTransportResult { Ok = false, Status = 400 });
+
+        var client = CreateClient();
+
+        await client.Authorization.RenameRoleAsync(new RenameRoleRequest
+        {
+            Id = RoleId.From(Guid.NewGuid()),
+            Name = "fail"
+        });
+
+        _events.Verify(x => x.PublishAsync(It.IsAny<UAuthStateEventArgs>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task SetRolePermissions_Should_Publish_Event_On_Success()
+    {
+        _request.Setup(x => x.SendJsonAsync(It.IsAny<string>(), It.IsAny<object>()))
+            .ReturnsAsync(new UAuthTransportResult { Ok = true, Status = 200 });
+
+        var client = CreateClient();
+
+        var request = new SetRolePermissionsRequest
+        {
+            RoleId = RoleId.From(Guid.NewGuid()),
+            Permissions = new List<Permission> { Permission.From("read"), Permission.From("write") }
+        };
+
+        await client.Authorization.SetRolePermissionsAsync(request);
+
+        _events.Verify(x => x.PublishAsync(It.Is<UAuthStateEventArgs>(e => e.Type == UAuthStateEvent.AuthorizationChanged)), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteRole_Should_Publish_Event_On_Success()
+    {
+        _request.Setup(x => x.SendJsonAsync(It.IsAny<string>(), It.IsAny<object>()))
+            .ReturnsAsync(SuccessJson(new DeleteRoleResult()));
+
+        var client = CreateClient();
+
+        var request = new DeleteRoleRequest
+        {
+            Id = RoleId.From(Guid.NewGuid())
+        };
+
+        await client.Authorization.DeleteRoleAsync(request);
+        _events.Verify(x => x.PublishAsync(It.Is<UAuthStateEventArgs>(e => e.Type == UAuthStateEvent.AuthorizationChanged)), Times.Once);
     }
 }
