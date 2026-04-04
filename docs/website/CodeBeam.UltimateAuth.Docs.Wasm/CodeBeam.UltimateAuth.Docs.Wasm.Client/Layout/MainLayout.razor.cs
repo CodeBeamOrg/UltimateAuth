0154cbe614fb0127d66552755911e472690abf8a
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2026-04-04 21:30:39 +0300

    Enhance Home Page

--- docs/website/CodeBeam.UltimateAuth.Docs.Wasm/CodeBeam.UltimateAuth.Docs.Wasm.Client/Layout/MainLayout.razor.cs
+++ docs/website/CodeBeam.UltimateAuth.Docs.Wasm/CodeBeam.UltimateAuth.Docs.Wasm.Client/Layout/MainLayout.razor.cs
@@ -6,21 +6,29 @@ public partial class MainLayout
 {
     private bool _drawerOpen = false;
     private bool _isDarkMode = true;
+    private bool _visionOverlay = false;
+
+    public void SetVisionOverlay(bool value)
+    {
+        _visionOverlay = value;
+        StateHasChanged();
+    }
 
     MudTheme _uauthTheme = new MudTheme()
     {
         PaletteLight = new PaletteLight()
         {
             Primary = "#0C1618",
-            PrimaryContrastText = "#FEEE88",
-
             Secondary = "#f6f5ae",
+            Tertiary = "#8CE38C",
         },
 
         PaletteDark = new PaletteDark()
         {
             Primary = "#FBFEFB",
+            PrimaryContrastText = "#0C1618",
             Secondary = "#2E2D4D",
+            Tertiary = "#8CE38C",
 
             TextPrimary = "#FBFEFB",
             TextSecondary = "#DEF7DE",
