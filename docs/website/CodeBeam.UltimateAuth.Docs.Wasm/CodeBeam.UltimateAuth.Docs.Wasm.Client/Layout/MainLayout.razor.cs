b288ebb92a2db35580eec579a0647713a5e62ab3
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2026-04-04 16:41:57 +0300

    Website Docs

---
+++ docs/website/CodeBeam.UltimateAuth.Docs.Wasm/CodeBeam.UltimateAuth.Docs.Wasm.Client/Layout/MainLayout.razor.cs
@@ -0,0 +1,31 @@
+﻿using MudBlazor;
+
+namespace CodeBeam.UltimateAuth.Docs.Wasm.Client.Layout;
+
+public partial class MainLayout
+{
+    private bool _drawerOpen = false;
+    private bool _isDarkMode = true;
+
+    MudTheme _uauthTheme = new MudTheme()
+    {
+        PaletteLight = new PaletteLight()
+        {
+            Primary = "#0C1618",
+            PrimaryContrastText = "#FEEE88",
+
+            Secondary = "#f6f5ae",
+        },
+
+        PaletteDark = new PaletteDark()
+        {
+            Primary = "#FBFEFB",
+            Secondary = "#2E2D4D",
+
+            TextPrimary = "#FBFEFB",
+            TextSecondary = "#DEF7DE",
+
+            Background = "#0C1618"
+        },
+    };
+}
---
+++ docs/website/CodeBeam.UltimateAuth.Docs.Wasm/CodeBeam.UltimateAuth.Docs.Wasm/Program.cs
@@ -0,0 +1,35 @@
+using CodeBeam.UltimateAuth.Docs.Wasm.Client.Pages;
+using CodeBeam.UltimateAuth.Docs.Wasm.Components;
+using MudBlazor.Services;
+using MudExtensions.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
+builder.Services.AddMudExtensions();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(CodeBeam.UltimateAuth.Docs.Wasm.Client._Imports).Assembly);
+
+app.Run();
