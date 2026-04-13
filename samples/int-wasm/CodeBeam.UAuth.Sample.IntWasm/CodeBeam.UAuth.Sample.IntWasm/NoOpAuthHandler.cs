7da3be9f0ce942b9933dc445bdddbee8b82dd897
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2026-04-13 20:27:53 +0300

    Cleanup

--- samples/int-wasm/CodeBeam.UAuth.Sample.IntWasm/CodeBeam.UAuth.Sample.IntWasm/NoOpAuthHandler.cs
+++ samples/int-wasm/CodeBeam.UAuth.Sample.IntWasm/CodeBeam.UAuth.Sample.IntWasm/NoOpAuthHandler.cs
@@ -9,9 +9,8 @@ public class NoopAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions
     public NoopAuthHandler(
         IOptionsMonitor<AuthenticationSchemeOptions> options,
         ILoggerFactory logger,
-        UrlEncoder encoder,
-        ISystemClock clock)
-        : base(options, logger, encoder, clock)
+        UrlEncoder encoder)
+        : base(options, logger, encoder)
     {
     }
 
