8256d65448cfd3b64f9dd77354a52fc781959a4b
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2026-04-20 16:30:23 +0300

    Update 0.1.0-rc.1 (#45)

---
+++ samples/CodeBeam.UltimateAuth.Sample.Seed/Extensions/UAuthSeedExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using CodeBeam.UltimateAuth.Core.Infrastructure;
+using CodeBeam.UltimateAuth.Core.MultiTenancy;
+
+namespace CodeBeam.UltimateAuth.Sample.Seed.Extensions;
+
+public static class UAuthSeedExtensions
+{
+    public static async Task SeedUltimateAuthAsync(this WebApplication app, TenantKey? tenant = null, CancellationToken ct = default)
+    {
+        using var scope = app.Services.CreateScope();
+        var runner = scope.ServiceProvider.GetRequiredService<SeedRunner>();
+        await runner.RunAsync(tenant, ct);
+    }
+}
