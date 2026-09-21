ba3370a09bc96a9739a13771ab78d6c8fb467284
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2026-09-21 22:53:49 +0300

    Credential Store Tests

---
+++ tests/CodeBeam.UltimateAuth.Tests.Unit/Credentials/Store/IPasswordCredentialStoreTestDatabase.cs
@@ -0,0 +1,9 @@
+﻿using CodeBeam.UltimateAuth.Core.MultiTenancy;
+using CodeBeam.UltimateAuth.Credentials.Reference;
+
+namespace CodeBeam.UltimateAuth.Tests.Unit.Credentials.Contracts;
+
+public interface IPasswordCredentialStoreTestDatabase : IAsyncDisposable
+{
+    IPasswordCredentialStore CreateStore(TenantKey tenant);
+}
